56e11e1f9edc068ea07326f5ac6947306dc32d22
jpeveri <jpeveri@solstice.com>
2018-10-25 12:05:13 -0300

    replaced template with my own page

---
+++ js/code.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+    var scrollTop = 0;
+    $(window).scroll(function(){
+        scrollTop = $(window).scrollTop();
+        if (scrollTop >= 50) {
+            $('nav').addClass('reduced');
+        } else if (scrollTop < 50) {
+            $('nav').removeClass('reduced');
+        }
+    });
+
+    /*-------------------------------------------------------------------------------
+    smoothScroll js
+  -------------------------------------------------------------------------------*/
+
+    $(function() {
+        $('.nav-item a, #home a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+});
