3ee13f6381a940a8a82b49f4a9aa46ef6a2074ca
jpprodriguez <jpprodriguez@github.com>
2019-12-23 12:17:23 -0300

    Added navbar menu for mobile

--- js/code.js
+++ js/code.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     var scrollTop = 0;
+    var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     $(window).scroll(function(){
         scrollTop = $(window).scrollTop();
-        if (scrollTop >= 50) {
+        if (scrollTop >= 50 && viewportWidth > 575) {
             $('nav').addClass('reduced');
         } else if (scrollTop < 50) {
             $('nav').removeClass('reduced');
@@ -20,6 +21,18 @@ $(document).ready(function(){
                 scrollTop: $($anchor.attr('href')).offset().top - 49
             }, 1000);
             event.preventDefault();
+        });
+    });
+
+    $(function() {
+        $('.navbar-button').bind('click', function(event) {
+            if($('nav').hasClass('closed')) {
+                $('nav').removeClass('closed');
+                $('nav').addClass('opened');
+            } else {
+                $('nav').removeClass('opened');
+                $('nav').addClass('closed');
+            }
         });
     });
 
